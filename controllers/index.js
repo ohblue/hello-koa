2024104887ff032e342bc1f61695f36ada87977f
BenC <ohblue@qq.com>
2019-08-03 08:05:44 +0800

    modify title

--- controllers/index.js
+++ controllers/index.js
@@ -1,5 +1,5 @@
 var fn_index = async (ctx, next) => {
-    ctx.response.body = `<h1>Index</h1>
+    ctx.response.body = `<h1>Hello world!</h1>
         <form action="/signin" method="post">
             <p>Name: <input name="name" value="koa"></p>
             <p>Password: <input name="password" type="password"></p>
