1639892d0361a1f757e38e0e88debf6c3a49b6b9
BenC <ohblue@qq.com>
2019-08-03 08:04:55 +0800

    modify title

--- controllers/index.js
+++ controllers/index.js
@@ -13,7 +13,7 @@ var fn_signin = async (ctx, next) => {
         password = ctx.request.body.password || '';
     console.log(`signin with name: ${name}, password: ${password}`);
     if (name === 'koa' && password === '12345') {
-        ctx.response.body = `<h1>Welcome, ${name}!</h1>`;
+        ctx.response.body = `<h1>Hello word, Welcome, ${name}!</h1>`;
     } else {
         ctx.response.body = `<h1>Login failed!</h1>
         <p><a href="/">Try again</a></p>`;
